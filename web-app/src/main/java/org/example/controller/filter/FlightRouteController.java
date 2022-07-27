904127b39c83ead1904a6e106911901462812093
anomalmishka <red_scorpions@mail.ru>
2022-07-27 23:55:39 +0300

    modify web-app

--- web-app/src/main/java/org/example/controller/filter/FlightRouteController.java
+++ web-app/src/main/java/org/example/controller/filter/FlightRouteController.java
@@ -9,39 +9,33 @@ import org.example.mapper.page.FlightRouteMapperPage;
 import org.example.service.filter.flightRoute.FlightRouteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@Controller
-@RequestMapping(path = "find")
+//@RequiredArgsConstructor
+//@Controller
+//@RequestMapping(path = "find")
 public class FlightRouteController {
 
-    private final FlightRouteService flightRouteService;
-    private final FlightRouteMapperModif flightRouteMapperModif;
-    private final FlightRouteMapperPage flightRouteMapperPage;
-    private final FilterMapper filterMapper;
+//    private final FlightRouteService flightRouteService;
+//    private final FlightRouteMapperModif flightRouteMapperModif;
+//    private final FlightRouteMapperPage flightRouteMapperPage;
+//    private final FilterMapper filterMapper;
 
-    @GetMapping("route")
-    public String getFindRoutePage() {
-        return "pages/findRoute";
-    }
 
-    @PostMapping("route")
-    public String FindRoute(Model model,
-                            @ModelAttribute FilterDTO filterDTO) {
-        List<FlightRoutePage> flightRoutePageList =
-                flightRouteMapperPage.toPageDTO(flightRouteMapperModif
-                        .toDTO(flightRouteService
-                                .findRoute(filterMapper
-                                        .toModel(filterDTO))));
-        model.addAttribute("flightRouteDTOModifPageList", flightRoutePageList);
-        return "pages/findRoute";
-    }
+//    @PostMapping("route")
+//    public String getFindRoutePage(Model model,
+//                                   @ModelAttribute FilterDTO filterDTO) {
+//        System.out.println(filterDTO);
+//        List<FlightRoutePage> flightRoutePageList =
+//                flightRouteMapperPage.toPageDTO(flightRouteMapperModif
+//                        .toDTO(flightRouteService
+//                                .findRoute(filterMapper
+//                                        .toModel(filterDTO))));
+//        model.addAttribute("flightRouteDTOModifPageList", flightRoutePageList);
+//        return "redirect:"+"/";
+//    }
 
 }
 
